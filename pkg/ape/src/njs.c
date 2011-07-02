44126848a89b835bb13528da2ff731ba487a1c9b
niteloser (Mr ) <niteloserpopescu@gmail.com>
2011-07-02 14:02:33 +0000

    more debugging of njs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ape@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ape/src/njs.c
+++ pkg/ape/src/njs.c
@@ -58,9 +58,10 @@ void choosePair(double* D,int n,double* R,int* s, int* sw, int* x, int* y,int fS
                     }
           }
       }
+
     //no missing distances, just return the one with maximal Q-criterion
    if(sww==0){*x=iFS[0];*y=jFS[0];*sw=0;return;
-            }
+             }
    /*for(k=0;k<fS;k++)
               {Rprintf("value[%i]=%f ",k,cFS[k]);
                Rprintf("i=%i ",iFS[k]);
@@ -148,7 +149,7 @@ void choosePair(double* D,int n,double* R,int* s, int* sw, int* x, int* y,int fS
     if(nb>max){max=nb;}
     cFS[i]=nb;
   }
-  /*Rprintf("all m*xy values\n");
+ /*Rprintf("all m*xy values\n");
  for(k=0;k<fS;k++)
               {Rprintf("value[%i]=%f ",k,cFS[k]);
                Rprintf("i=%i ",iFS[k]);
@@ -203,14 +204,14 @@ double cnxy(int x, int y, int n,double* D)
     double nMeanXY=0;
     //Rprintf("cN[%i,%i]\n",x,y);
     for(i=1;i<=n;i++)
-     {
+     {if(i==x || i==y)continue;
       for(j=1;j<=n;j++)
       {if(i==j)continue;
-       if((i==x && j==y) || (i==y && j==x))continue;
+       if(j==y || j==x)continue;
        double n1=0;
        double n2=0;
-       if(i!=x)n1=D[give_index(i,x,n)];
-       if(j!=y)n2=D[give_index(j,y,n)];
+       n1=D[give_index(i,x,n)];
+       n2=D[give_index(j,y,n)];
        if(n1==-1 || n2==-1 || D[give_index(i,j,n)]==-1)continue;
        nMeanXY+=(n1+n2-D[give_index(x,y,n)]-D[give_index(i,j,n)]);
        //Rprintf("cnMeanXY after (%i,%i)=%f\n",i,j,nMeanXY);
@@ -275,14 +276,14 @@ double nxy(int x, int y, int n,double* D)
     double nMeanXY=0;
     //Rprintf("N[%i,%i]\n",x,y);
     for(i=1;i<=n;i++)
-     {
+     {if(i==x || i==y)continue;
       for(j=1;j<=n;j++)
       {if(i==j)continue;
-       if((i==x && j==y) || (i==y && j==x))continue;
+       if(j==x || j==y)continue;
        double n1=0;
        double n2=0;
-       if(i!=x)n1=D[give_index(i,x,n)];
-       if(j!=y)n2=D[give_index(j,y,n)];
+       n1=D[give_index(i,x,n)];
+       n2=D[give_index(j,y,n)];
        if(n1==-1 || n2==-1 || D[give_index(i,j,n)]==-1)continue;
        sCXY++;
        //Rprintf("considered pair (%i,%i)\n",i,j);
@@ -290,6 +291,7 @@ double nxy(int x, int y, int n,double* D)
        //Rprintf("nMeanXY after (%i,%i)=%f\n",i,j,nMeanXY);
       }
      }
+    //Rprintf("sCXY=%i",sCXY);
     return nMeanXY/sCXY;
 }
 
@@ -300,10 +302,10 @@ int cxy(int x, int y, int n,double* D)
     int j=0;
     
     for(i=1;i<=n;i++)
-     {
+     {if(i==x || i==y)continue;
       for(j=1;j<=n;j++)
       {if(i==j)continue;
-       if((i==x && j==y) || (i==y && j==x))continue;
+       if(j==x || j==y)continue;
        double n1=0;
        double n2=0;
        if(i!=x)n1=D[give_index(i,x,n)];
@@ -377,7 +379,7 @@ void njs(double *D, int *N, int *edge1, int *edge2, double *edge_length)
            }
          }
 
-       /* for(i=1;i<n;i++)
+        /*for(i=1;i<n;i++)
                   {
                     for(j=i+1;j<=n;j++)
                       {
