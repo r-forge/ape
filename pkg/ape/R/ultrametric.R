56a68004b5a8f89ad7cb52b2248226998a6a0a2d
niteloser (Mr ) <niteloserpopescu@gmail.com>
2011-08-03 10:44:24 +0000

    additive and ultrametric procedure for filling missing distance matrices
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ape@41 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/ape/R/ultrametric.R
@@ -0,0 +1,14 @@
+ultrametric <- function(X)
+{
+    if (is.matrix(X)) X <- as.dist(X)
+    X[is.na(X)]=-1
+    X[X<0]=-1	
+    X[is.nan(X)]=-1
+    N <- attr(X, "Size")
+    labels <- attr(X, "Labels")
+    if (is.null(labels)) labels <- as.character(1:N)
+    m<-length(which(is.na(d)))
+    ans<- .C("ultrametric",as.double(X),as.integer(N),as.integer(m),double(N*N))
+    ret<-matrix(ans[[4]],N,N)   
+  ret 
+}
